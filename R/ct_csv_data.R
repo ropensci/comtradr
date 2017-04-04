69704ee7862740f0e60ca7be8b0936f811edda30
Chris Muir <chrismuirRVA@gmail.com>
2017-04-03 21:44:52 -0400

    do not export func to NAMESPACE

--- R/ct_csv_data.R
+++ R/ct_csv_data.R
@@ -1,48 +1,47 @@
 #' UN Comtrade data extraction via CSV
-#' 
-#' Function makes an API call to UN Comtrade, data is returned from the API as 
-#' a CSV object. Return value of this function is a list containing the data 
-#' as a dataframe and information on whether the connection to the API was 
-#' successful, and whether or not data was returned from the API. For use with 
-#' the UN Comtrade API, full API docs can be found at 
+#'
+#' Function makes an API call to UN Comtrade, data is returned from the API as
+#' a CSV object. Return value of this function is a list containing the data
+#' as a dataframe and information on whether the connection to the API was
+#' successful, and whether or not data was returned from the API. For use with
+#' the UN Comtrade API, full API docs can be found at
 #' \url{https://comtrade.un.org/data/doc/api/}
 #'
 #' @param url Complete url string of the call to the API.
-#' @param colname Indication as to whether to use "human friendly" col names or 
+#' @param colname Indication as to whether to use "human friendly" col names or
 #'  "machine friendly" col names. Value will be either "H" or "M".
 #'
 #' @return List of length three, elements are:
 #'  \itemize{
 #'  \item \code{msg}: Brief message on whether data was returned.
-#'  \item \code{details}: Further details related to the nature of the API 
+#'  \item \code{details}: Further details related to the nature of the API
 #'    return.
 #'  \item \code{data}: Dataframe object of return data.
 #'  }
-#' @export
 ct_csv_data <- function(url, colname) {
-  
-  rawdata <- tryCatch(read.csv(url, stringsAsFactors = FALSE), 
+
+  rawdata <- tryCatch(utils::read.csv(url, stringsAsFactors = FALSE),
                       error = function(e) e)
-  
-  if (is(rawdata, "error")) {
+
+  if (methods::is(rawdata, "error")) {
     msg <- "No data returned"
     details <- "Could not complete connection to API"
     return(list(msg = msg, details = details, data = NULL))
   }
-  
-  if (grepl("No data matches", rawdata$Classification[1], 
+
+  if (grepl("No data matches", rawdata$Classification[1],
             ignore.case = TRUE)) {
     msg <- "No data returned"
     details <- "Connection successful"
     return(list(msg = msg, details = details, data = NULL))
   }
-  
+
   if (ncol(rawdata) == 1) {
     msg <- "No data returned, param error within API function call"
     details <- rawdata[[1]][1]
     return(list(msg = msg, details = details, data = NULL))
   }
-  
+
   if (colname == "H") {
     colnames(rawdata) <- api_col_names()
   }
--- R/ct_json_data.R
+++ R/ct_json_data.R
@@ -1,51 +1,50 @@
 #' UN Comtrade data extraction via JSON
-#' 
-#' Function makes an API call to UN Comtrade, data is returned from the API as 
-#' a JSON object. Return value of this function is a list containing the data 
-#' as a dataframe and information on whether the connection to the API was 
-#' successful, and whether or not data was returned from the API. For use with 
-#' the UN Comtrade API, full API docs can be found at 
+#'
+#' Function makes an API call to UN Comtrade, data is returned from the API as
+#' a JSON object. Return value of this function is a list containing the data
+#' as a dataframe and information on whether the connection to the API was
+#' successful, and whether or not data was returned from the API. For use with
+#' the UN Comtrade API, full API docs can be found at
 #' \url{https://comtrade.un.org/data/doc/api/}
 #'
 #' @param url Complete url string of the call to the API.
-#' @param colname Indication as to whether to use "human friendly" col names or 
+#' @param colname Indication as to whether to use "human friendly" col names or
 #'  "machine friendly" col names. Value will be either "H" or "M".
 #'
 #' @return List of length three, elements are:
 #'  \itemize{
 #'  \item \code{msg}: Brief message on success/failure of the API call.
-#'  \item \code{details}: More detailed message on success/failure of the API 
+#'  \item \code{details}: More detailed message on success/failure of the API
 #'    call.
 #'  \item \code{data}: Dataframe object of return data.
 #'  }
 #' @importFrom dplyr "%>%"
-#' @export
 ct_json_data <- function(url, colname) {
-  
+
   rawdata <- tryCatch(httr::GET(url), error = function(e) e)
-  
-  if (is(rawdata, "error")) {
+
+  if (methods::is(rawdata, "error")) {
     msg <- "Could not complete connection to API"
     details <- NULL
     return(list(msg = msg, details = details, data = NULL))
   }
-  
+
   if (httr::http_error(rawdata)) {
     msg <- "Could not complete connection to API"
     details <- httr::status_code(rawdata)
     return(list(msg = msg, details = details, data = NULL))
   }
-  
+
   if (httr::http_type(rawdata) != "application/json") {
     msg <- "API did not return a json object"
     details <- httr::http_type(rawdata)
     return(list(msg = msg, details = details, data = NULL))
   }
-  
-  rawdata <- rawdata %>% 
-    httr::content("text", encoding = "UTF-8") %>% 
+
+  rawdata <- rawdata %>%
+    httr::content("text", encoding = "UTF-8") %>%
     jsonlite::fromJSON(simplifyDataFrame = TRUE)
-  
+
   if (length(rawdata$dataset) == 0) {
     msg <- rawdata$validation$status$name
     details <- rawdata$validation$message
@@ -58,6 +57,6 @@ ct_json_data <- function(url, colname) {
     details <- "Connection successful"
     df <- rawdata$dataset
   }
-  
+
   return(list(msg = msg, details = details, data = df))
 }
