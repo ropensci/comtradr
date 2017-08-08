fe0522ad78db5c372354c72e8c0be02307d2b2e1
Chris Muir <chrismuirRVA@gmail.com>
2017-08-07 21:35:58 -0400

    update tests to run on travis

--- tests/testthat/test-country_lookup.R
+++ tests/testthat/test-country_lookup.R
@@ -2,7 +2,7 @@ context("country_lookup")
 
 # All tests on the expected return data.
 test_that("lookup return values are correct, and fail when expected", {
-  skip_on_cran()
+  #skip_on_cran()
   #skip_on_travis()
 
   df <- ct_countries_table()
--- tests/testthat/test-ct_commodities_table.R
+++ tests/testthat/test-ct_commodities_table.R
@@ -2,7 +2,7 @@ context("ct_commodities_table")
 
 # All tests on the expected return data.
 test_that("return has correct attributes, and fails when expected", {
-  skip_on_cran()
+  #skip_on_cran()
   #skip_on_travis()
 
   df <- ct_commodities_table(type = "HS")
