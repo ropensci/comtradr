5ee0fe1cdf976e74786c28290087ea61ebe45f25
ChrisMuir <chrismuirRVA@gmail.com>
2017-09-20 19:17:22 -0400

    update docs to mention use of regex, issue #4

--- R/ct_commodity_lookup.R
+++ R/ct_commodity_lookup.R
@@ -4,11 +4,11 @@
 #' using commodity codes. This is a helper function for querying the
 #' Comtrade commodity database. It takes as input a vector of
 #' commodities or commodity codes. Output is a list or vector of commodity
-#' descriptions or codes associated with the input values. For use with the UN
-#' Comtrade API, full API docs can be found at
+#' descriptions or codes associated with the input search_terms. For use with
+#' the UN Comtrade API, full API docs can be found at
 #' \url{https://comtrade.un.org/data/doc/api/}
 #'
-#' @param values Commodity names or commodity codes, as a char or numeric
+#' @param search_terms Commodity names or commodity codes, as a char or numeric
 #'  vector.
 #' @param return_code Logical, if set to FALSE, the function will return a
 #'  set of commodity descriptions along with commodity codes (as a single
@@ -18,12 +18,21 @@
 #'  matches as a named list, if set to TRUE it will return them as a character
 #'  vector. Default value is FALSE.
 #' @param verbose Logical, if set to TRUE, a warning message will print to
-#'  console if any of the elements of input "values" returned no matches
+#'  console if any of the elements of input "search_terms" returned no matches
 #'  (message will indicate which elements returned no data). Default is TRUE.
 #'
 #' @return A list or character vector of commodity descriptions and/or
-#'  commodity codes that are matches with the elements of "values".
+#'  commodity codes that are matches with the elements of "search_terms".
+#'
+#' @details This function uses regular expressions (regex) to find matches
+#'  within the commodity DB. This means it will treat as a match any commodity
+#'  description that contains the input search term. For more on using regex
+#'  within R, see this great tutorial by Gloria Li and Jenny Bryan
+#'  \url{http://stat545.com/block022_regular-expression.html}
+#'
 #' @export
+#'
+#' @seealso \code{\link{grepl}}
 #'
 #' @examples \dontrun{
 #' # Look up commodity descriptions related to "halibut"
@@ -41,10 +50,10 @@
 #'                     verbose = TRUE)
 #' }
 
-ct_commodity_lookup <- function(values, return_code = FALSE,
+ct_commodity_lookup <- function(search_terms, return_code = FALSE,
                                 return_char = FALSE, verbose = TRUE) {
-  stopifnot(mode(values) %in% c("numeric", "character"))
-  values <- as.character(values)
+  stopifnot(mode(search_terms) %in% c("numeric", "character"))
+  search_terms <- as.character(search_terms)
 
   # Fetch the commodity database from ct_env.
   commodity_df <- get_commodity_db()
@@ -58,9 +67,9 @@ ct_commodity_lookup <- function(values, return_code = FALSE,
     return_col <- "commodity"
   }
 
-  # For each element of input arg "values", fetch all commodity descriptions
-  # and/or codes from the database. Output will be a list.
-  ans <- purrr::map(values, function(x) {
+  # For each element of input arg "search_terms", fetch all commodity
+  # descriptions and/or codes from the database. Output will be a list.
+  ans <- purrr::map(search_terms, function(x) {
     # Determine whether the param 'value' is a commodity or a code, then
     # perform the look up.
     if (grepl("[a-z]", x)) {
@@ -71,14 +80,16 @@ ct_commodity_lookup <- function(values, return_code = FALSE,
     commodity_df[grepl(x, commodity_df[[lu]], ignore.case = TRUE), return_col]
   })
 
-  # If "verbose" == TRUE, create warning message if any of the elements of input
-  # arg "values" produced no search results.
+  # If "verbose" == TRUE, create warning message if any of the elements of
+  # input arg "search_terms" produced no search results.
   if (verbose) {
     check_len <- purrr::map_int(ans, length)
     if (any(check_len == 0)) {
       if (any(check_len > 0)) {
-        msg <- paste0("There were no matching results found for inputs: ",
-                      paste(values[which(check_len == 0)], collapse = ", "))
+        msg <- paste0(
+          "There were no matching results found for inputs: ",
+          paste(search_terms[which(check_len == 0)], collapse = ", ")
+        )
       } else {
         msg <- "There were no matching results found"
       }
@@ -87,11 +98,11 @@ ct_commodity_lookup <- function(values, return_code = FALSE,
   }
 
   # If "return_char" == TRUE, unlist obj "ans". Otherwise, assign names to the
-  # elements of obj "ans" (names will be taken from input arg "values").
+  # elements of obj "ans" (names will be taken from input arg "search_terms").
   if (return_char) {
     ans <- unlist(ans, FALSE, FALSE)
   } else {
-    names(ans) <- values
+    names(ans) <- search_terms
   }
   return(ans)
 }
--- R/ct_country_lookup.R
+++ R/ct_country_lookup.R
@@ -4,33 +4,39 @@
 #' spelling/capitalization. This is a helper function for querying the country
 #' names/spelling used by Comtrade.. It takes as input a vector of
 #' country names, output is any country names that contain any of the input
-#' strings, using regex via the base function grepl (search is case insensitive).
+#' strings, using regex via the base function grepl.
 #' For use with the UN Comtrade API, full API docs can be found at
 #' \url{https://comtrade.un.org/data/doc/api/}
 #'
-#' @param loc Char vector of country names.
+#' @param search_terms Char vector of country names.
 #' @param type The country list to use for the search, valid inputs are
 #'  "reporter" and "partner".
 #'
 #' @return A character vector of country names that are complete or partial
 #'  matches with any of the input country names.
+#'
+#' @details This function uses regular expressions (regex) to find matches
+#'  within the country DB. This means it will treat as a match any country
+#'  string that contains the input search term. For more on using regex
+#'  within R, see this great tutorial by Gloria Li and Jenny Bryan
+#'  \url{http://stat545.com/block022_regular-expression.html}
+#'
 #' @export
+#'
+#' @seealso \code{\link{grepl}}
 #'
 #' @examples
 #' # Look up all reporters that contain the terms "korea" and "vietnam"
 #' ct_country_lookup(c("korea", "vietnam"), "reporter")
-ct_country_lookup <- function(loc, type = c("reporter", "partner")) {
+ct_country_lookup <- function(search_terms, type = c("reporter", "partner")) {
   # Input validation.
   type <- match.arg(type)
-  if (!is.character(loc) || length(loc) < 1) {
-    stop("param 'loc' must be a char vector with length greater than zero",
-         call. = FALSE)
-  }
+  stopifnot(is.character(search_terms))
 
-  # If length of loc is more than one, transform values into a regex friendly
-  # string.
-  if (length(loc) > 1) {
-    loc <- paste(loc, collapse = "|")
+  # If length of search_terms is more than one, transform values into a regex
+  # friendly string.
+  if (length(search_terms) > 1) {
+    search_terms <- paste(search_terms, collapse = "|")
   }
 
   # Fetch the country databse from ct_env.
@@ -38,7 +44,7 @@ ct_country_lookup <- function(loc, type = c("reporter", "partner")) {
 
   # Perform the lookup.
   ans <- country_df[country_df$type == type &
-                      grepl(loc, country_df$`country name`,
+                      grepl(search_terms, country_df$`country name`,
                             ignore.case = TRUE),
                     c("country name")]
 
