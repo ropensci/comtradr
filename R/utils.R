a6f9c4a432db7e0fbb2ad304c6cd6b19e5654b0f
Paul Bochtler <paulbochtler@MacBook-Pro-von-Paul.local>
2023-04-18 01:03:20 +0200

    added simple auth helpers

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,37 @@
+#' Set your primary Comtrade API key in the environment variable
+#'
+#' If you would like your Comtrade API key to persist in between sessions, use `usethis::edit_r_environ()` to add the env variable COMTRADE_PRIMARY to your environment file.
+#'
+#' @param key Provide your primary comtrade key
+#'
+#' @return Saves your comtrade primary key in the environment.
+#' @export
+set_primary_comtrade_key <- function(key = NULL) {
+  if (is.null(key)) {
+    key <- askpass::askpass("Please enter your API key")
+  }
+  Sys.setenv("COMTRADE_PRIMARY" = key)
+}
+
+#' get_primary_comtrade_key
+#'
+#' If you would like your Comtrade API key to persist in between sessions, use `usethis::edit_r_environ()` to add the env variable COMTRADE_PRIMARY to your environment file.
+#'
+#' @return Gets your primary comtrade key from the environment var COMTRADE_PRIMARY
+#' @export
+get_primary_comtrade_key <- function() {
+  key <- Sys.getenv("COMTRADE_PRIMARY")
+  if (!identical(key, "")) {
+    return(key)
+  } else {
+    rlang::abort("No API key found, please supply with `set_primary_comtrade_key` function or set COMTRADE_PRIMARY env var")
+  }
+}
+
+
+
+
+
 #' Get commodity database
 #'
 #' Helper function that attempts to return the commodity DB as a data frame.
@@ -91,3 +125,4 @@ commodity_type_switch <- function(commodity_type) {
 
   dict[commodity_type]
 }
+
