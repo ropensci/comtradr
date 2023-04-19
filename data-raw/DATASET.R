cb5f4d09cdf0fb758a455be9bc1ea903f6206ea8
Paul Bochtler <paulbochtler@MacBook-Pro-von-Paul.local>
2023-04-19 23:32:05 +0200

    create DATASET.R which queries all reference tables and stores them in extdata

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,32 +1,56 @@
 library(httr2)
 
 # getting comtrade data ---------------------------------------------------
-list_of_datasets <- httr2::request('https://comtradeapi.un.org/files/v1/app/reference/ListofReferences.json') |>
-  httr2::req_perform() |>
+
+## getting list of reference tables
+response <- httr2::request('https://comtradeapi.un.org/files/v1/app/reference/ListofReferences.json') |>
+  httr2::req_perform()
+
+## getting data from response of list of tables
+list_of_datasets <- response |>
   httr2::resp_body_json(simplifyVector = T) |>
   purrr::pluck(1)
 
-list_of_datasets$category <- stringr::str_replace_all(list_of_datasets$category,':',"_") |>  tolower()
+## getting date of last modification from list of tables
+last_modified <- httr2::resp_header(header = "Last-Modified", resp = response) |>
+  stringr::str_extract(pattern = '(\\d{2} [a-zA-Z]+ \\d{4})') |>
+  as.Date(format = "%d %b %Y")
+
+## writing last modification to data
+list_of_datasets$last_modified <- last_modified
+
+## changing colon to underscore in dataset names and adding prefix 'ref'
+list_of_datasets$category <- stringr::str_replace_all(list_of_datasets$category,':',"_") |>
+  tolower() |>
+  stringr::str_c('ref_',y =_)
+
 save(list_of_datasets, file = 'inst/extdata/list_of_datasets.rda')
 
 for(i in 1:nrow(list_of_datasets)){
 
     response <- httr2::request(list_of_datasets$fileuri[i]) |>
-    httr2::req_perform() |>
+    httr2::req_perform()
+
+    data <- response |>
     httr2::resp_body_json(simplifyVector = T)
-  print(length(response))
-  if(length(response)==1){
-    result <- response[[1]]
-  } else if(length(response)==2) {
-    result <- response[[2]]
+
+    last_modified <- httr2::resp_header(header = "Last-Modified", resp = response) |>
+      stringr::str_extract(pattern = '(\\d{2} [a-zA-Z]+ \\d{4})') |>
+      as.Date(format = "%d %b %Y")
+
+  print(length(data))
+  if(length(data)==1){
+    result <- data[[1]]
+  } else if(length(data)==2) {
+    result <- data[[2]]
   } else {
-    result <- response[[5]]
+    result <- data[[5]]
   }
+
+  result$last_modified <- last_modified
+
   assign(list_of_datasets$category[i],result)
 
   save(list = list_of_datasets$category[i],
        file = paste0('inst/extdata/',list_of_datasets$category[i],'.rda'))
-
 }
-
-# test <- load("~/Desktop/code_projekte/comtradr/inst/extdata/cmd_h1.rda")
