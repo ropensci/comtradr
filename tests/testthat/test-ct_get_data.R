ca69316f6c5c01d45ca66c4c44e44617c5fc13c4
paul bochtler <65470117+datapumpernickel@users.noreply.github.com>
2023-05-01 23:53:11 +0200

    pass in 'test' token, so that checks can be run.

--- tests/testthat/test-ct_get_data.R
+++ tests/testthat/test-ct_get_data.R
@@ -9,6 +9,7 @@ httptest2::with_mock_dir("goods", {
                                         start_date = '2010',
                                         end_date = '2010',
                                         flow_direction = 'all',
+                                        primary_token = 'test',
                                         partner_2 = 'World'), 'data.frame')
   })
 })
@@ -23,6 +24,7 @@ httptest2::with_mock_dir("services", {
                                         start_date = '2010',
                                         end_date = '2010',
                                         flow_direction = 'all',
+                                        primary_token = 'test',
                                         partner_2 = 'World'), 'data.frame')
   })
 })
@@ -41,6 +43,7 @@ without_internet({
                   start_date = '2010',
                   end_date = '2010',
                   flow_direction = 'all',
+                  primary_token = 'test',
                   partner_2 = 'World'                )
               ,'https://comtradeapi.un.org/data/v1/get/C/A/HS?cmdCode=TOTAL&flowCode=M%2CX%2CRM%2CRX&partnerCode=32%2C280%2C276&reporterCode=156&period=2010&motCode=0&partner2Code=0&customsCode=C00&includeDesc=TRUE')
             })
@@ -59,6 +62,7 @@ without_internet({
                   start_date = '2010',
                   end_date = '2010',
                   flow_direction = 'all',
+                  primary_token = 'test',
                   partner_2 = 'World',verbose = T)
               ,'https://comtradeapi.un.org/data/v1/get/S/A/EB?cmdCode=200&flowCode=M%2CX%2CRM%2CRX&partnerCode=32%2C280%2C276&reporterCode=156&period=2010&motCode=0&partner2Code=0&customsCode=C00&includeDesc=TRUE')
             })
