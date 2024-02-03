94a74f58eb565a01a5ddf4bb4548444f4179062f
paul bochtler <65470117+datapumpernickel@users.noreply.github.com>
2024-02-04 00:23:15 +0100

    proper test with calling rscript in test files to test .onLoad without unlockBinding or detaching the package

--- tests/testthat/test-.onload.R
+++ tests/testthat/test-.onload.R
@@ -7,24 +7,57 @@ testthat::test_that("ct_perform_request is cached", {
   testthat::expect_equal(comtradr:::cache$info()$max_n, Inf)
   testthat::expect_true(dir.exists(rappdirs::user_cache_dir('comtradr')))
 })
-#
-# testthat::test_that("ct_perform_request is cached", {
-#   # detach("package:comtradr", unload = TRUE)
-#   Sys.setenv('COMTRADR_CACHE_MAX_SIZE' = 1)
-#   Sys.setenv('COMTRADR_CACHE_MAX_AGE' = 1)
-#   Sys.setenv('COMTRADR_CACHE_MAX_N' = 1)
-#   Sys.setenv('R_USER_CACHE_DIR' = 'cache_test')
-#   comtradr:::.onLoad()
-#   testthat::expect_true(memoise::is.memoised(
-#     comtradr:::ct_perform_request_cache))
-#   testthat::expect_equal(comtradr:::cache$info()$dir,
-#                          stringr::str_c(getwd(),
-#                                         '/cache_test/comtradr'))
-#   testthat::expect_equal(comtradr:::cache$info()$max_size, 1)
-#   testthat::expect_equal(comtradr:::cache$info()$max_age, 1)
-#   testthat::expect_equal(comtradr:::cache$info()$max_n, 1)
-#   testthat::expect_true(dir.exists(rappdirs::user_cache_dir('comtradr')))
-# })
+
+
+
+testthat::test_that("ct_perform_request is cached", {
+  testthat::expect_equal({
+    sc <- tempfile()
+    cat("library(comtradr)\ncomtradr:::cache$info()$dir", file = sc)
+    sub <- callr::rscript(sc, env = c(
+      COMTRADR_CACHE_MAX_SIZE = "1",
+      COMTRADR_CACHE_MAX_AGE = "1",
+      COMTRADR_CACHE_MAX_N = "1",
+      R_USER_CACHE_DIR = "cache_test"
+    ))
+    stringr::str_extract(sub$stdout, '"/.*comtradr') |>
+      stringr::str_remove('^"') |>
+      stringr::str_remove('/$')
+  }, stringr::str_c(getwd(), '/cache_test/comtradr'))
+})
+
+
+testthat::test_that("ct_perform_request cache parameters are set correctly", {
+  script_content <- "
+    library(comtradr)
+    cache_info <- comtradr:::cache$info()
+    cache_info_list <- list(
+      max_size = cache_info$max_size,
+      max_age = cache_info$max_age,
+      max_n = cache_info$max_n
+    )
+    cat(jsonlite::toJSON(cache_info_list))
+  "
+
+  script_file <- tempfile()
+  writeLines(script_content, script_file)
+
+  output <- callr::rscript(script_file, env = c(
+    COMTRADR_CACHE_MAX_SIZE = "1",
+    COMTRADR_CACHE_MAX_AGE = "1",
+    COMTRADR_CACHE_MAX_N = "1",
+    R_USER_CACHE_DIR = "cache_test"
+  ))
+
+  cache_info <- jsonlite::fromJSON(output$stdout)
+
+  testthat::expect_equal(cache_info$max_size, 1)
+  testthat::expect_equal(cache_info$max_age, 1)
+  testthat::expect_equal(cache_info$max_n, 1)
+})
+
+
+
 
 
 
