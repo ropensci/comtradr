26751723f7a8bef588242b7c222cbbad2531564d
paul bochtler <65470117+datapumpernickel@users.noreply.github.com>
2024-02-03 13:46:46 +0100

    add memoised perform_request function .onLoad, check for environment variables to configure cache

--- R/zzz.R
+++ R/zzz.R
@@ -12,13 +12,30 @@ assign(
   envir = ct_env
 )
 
-.onLoad <- function() {
-  if(Sys.getenv('comtradr.cache_dir')==""){
-    cache <- cachem::cache_disk(dir = rappdirs::user_cache_dir(appname = 'comtradr'))
-  } else {
-    cache <- cachem::cache_disk(dir = Sys.getenv('comtradr.cache_dir'))
-  }
-  ct_perform_request_cache <<- memoise::memoise(ct_perform_request,cache = cache)
+
+.onLoad <- function(libname, pkgname) {
+  max_size_env <- Sys.getenv("COMTRADR_CACHE_MAX_SIZE")
+  max_age_env <- Sys.getenv("COMTRADR_CACHE_MAX_AGE")
+  max_n_env <- Sys.getenv("COMTRADR_CACHE_MAX_N")
+
+  max_size <- ifelse(nzchar(max_size_env), eval(parse(text = max_size_env)), 1024 * 1024^2)
+  max_age <- ifelse(nzchar(max_age_env), eval(parse(text = max_age_env)), Inf)
+  max_n <- ifelse(nzchar(max_n_env), eval(parse(text = max_n_env)), Inf)
+
+  cache <- cachem::cache_disk(dir = rappdirs::user_cache_dir(appname = 'comtradr'),
+                              max_size = max_size,
+                              max_age = max_age,
+                              max_n = max_n)
+
+  ct_perform_request_cache <- memoise::memoise(ct_perform_request, cache = cache)
+
+  assign(x = "ct_perform_request_cache",
+         value = ct_perform_request_cache,
+         envir = rlang::ns_env("comtradr"))
+
+  assign(x = "cache",
+         value = cache,
+         envir = rlang::ns_env("comtradr"))
 }
 
 # Initialize placeholders for package data within ct_env.
