bb9b4e359203528152e8598f3d3c762402330ba6
paul bochtler <65470117+datapumpernickel@users.noreply.github.com>
2024-02-03 15:31:29 +0100

    skip the test with detach package, to check if that triggers covr to misread coverage statistics

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -8,20 +8,20 @@ testthat::test_that("ct_perform_request is cached", {
   testthat::expect_true(dir.exists(rappdirs::user_cache_dir('comtradr')))
 })
 
-testthat::test_that("ct_perform_request is cached", {
-  detach("package:comtradr", unload = TRUE)
-  Sys.setenv('COMTRADR_CACHE_MAX_SIZE' = 1)
-  Sys.setenv('COMTRADR_CACHE_MAX_AGE' = 1)
-  Sys.setenv('COMTRADR_CACHE_MAX_N' = 1)
-  Sys.setenv('R_USER_CACHE_DIR' = 'cache_test')
-  library(comtradr)
-  testthat::expect_true(memoise::is.memoised(
-    comtradr:::ct_perform_request_cache))
-  testthat::expect_equal(comtradr:::cache$info()$dir,
-                         stringr::str_c(getwd(),
-                                        '/cache_test/comtradr'))
-  testthat::expect_equal(comtradr:::cache$info()$max_size, 1)
-  testthat::expect_equal(comtradr:::cache$info()$max_age, 1)
-  testthat::expect_equal(comtradr:::cache$info()$max_n, 1)
-  testthat::expect_true(dir.exists(rappdirs::user_cache_dir('comtradr')))
-})
+# testthat::test_that("ct_perform_request is cached", {
+#   detach("package:comtradr", unload = TRUE)
+#   Sys.setenv('COMTRADR_CACHE_MAX_SIZE' = 1)
+#   Sys.setenv('COMTRADR_CACHE_MAX_AGE' = 1)
+#   Sys.setenv('COMTRADR_CACHE_MAX_N' = 1)
+#   Sys.setenv('R_USER_CACHE_DIR' = 'cache_test')
+#   library(comtradr)
+#   testthat::expect_true(memoise::is.memoised(
+#     comtradr:::ct_perform_request_cache))
+#   testthat::expect_equal(comtradr:::cache$info()$dir,
+#                          stringr::str_c(getwd(),
+#                                         '/cache_test/comtradr'))
+#   testthat::expect_equal(comtradr:::cache$info()$max_size, 1)
+#   testthat::expect_equal(comtradr:::cache$info()$max_age, 1)
+#   testthat::expect_equal(comtradr:::cache$info()$max_n, 1)
+#   testthat::expect_true(dir.exists(rappdirs::user_cache_dir('comtradr')))
+# })
