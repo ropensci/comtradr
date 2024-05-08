ccd90a4313883b77046fd39ba766329b7b95e5e4
datapumpernickel <65470117+datapumpernickel@users.noreply.github.com>
2024-05-08 17:31:46 +0200

    included specific build bulk request function in standard builid request function

--- R/ct_build_request.R
+++ R/ct_build_request.R
@@ -14,42 +14,59 @@
 #' @inheritParams ct_get_data
 ct_build_request <- function(params,
                              primary_token = NULL,
+                             file_hash = NA,
+                             reporter_code = NA,
                              verbose = FALSE,
                              bulk) {
-  query_params <- params$query_params
+  ## if the file_hash is missing we know that this is about the standard API
+  if(is.na(file_hash)){
+    query_params <- params$query_params
 
-  extra_params <- params$extra_params |>
-    purrr::map(unlist) |>
-    purrr::pluck(1) |>
-    as.list()
-  query_params <- c(query_params, extra_params)
-  type <- params$url_params$type
+    extra_params <- params$extra_params |>
+      purrr::map(unlist) |>
+      purrr::pluck(1) |>
+      as.list()
+    query_params <- c(query_params, extra_params)
+    type <- params$url_params$type
 
-  freq <- params$url_params$freq
+    freq <- params$url_params$freq
 
-  clCode <- params$url_params$clCode
+    clCode <- params$url_params$clCode
 
-  if(bulk){
-    base_url <- "https://comtradeapi.un.org/bulk/v1/get/"
+    if(bulk){
+      base_url <- "https://comtradeapi.un.org/bulk/v1/get/"
+    } else {
+      base_url <- "https://comtradeapi.un.org/data/v1/get/"
+    }
+
+    req <-
+      httr2::request(base_url) |>
+      httr2::req_url_path_append(type) |>
+      httr2::req_url_path_append(freq) |>
+      httr2::req_url_path_append(clCode) |>
+      httr2::req_headers(`Ocp-Apim-Subscription-Key` = primary_token) |>
+      httr2::req_url_query(!!!query_params)
+
+    if (stringr::str_length(req$url) > 4095) {
+      rlang::abort("Your request exceeds 4KB or 4096 characters, which is the upper limit of the Comtrade API.") # nolint
+    }
+
+    if (verbose) {
+      cli::cli_inform(c("i" = paste0("URL that will be queried: ", req$url)))
+    }
+
   } else {
-    base_url <- "https://comtradeapi.un.org/data/v1/get/"
+    ## in this case here we are constructing a special request for a bulk file
+    req <- httr2::request("https://comtradeapi.un.org/bulk/v1/file/") |>
+      httr2::req_url_path_append(list(reporter_code, file_hash)) |>
+      httr2::req_url_query(format = "json") |>
+      httr2::req_headers(`Ocp-Apim-Subscription-Key` = primary_token)
+
+    if (stringr::str_length(req$url) > 4095) {
+      rlang::abort("Your request exceeds 4KB or 4096 characters, which is the upper limit of the Comtrade API.") # nolint
+    }
   }
 
-  res <-
-    httr2::request(base_url) |>
-    httr2::req_url_path_append(type) |>
-    httr2::req_url_path_append(freq) |>
-    httr2::req_url_path_append(clCode) |>
-    httr2::req_headers(`Ocp-Apim-Subscription-Key` = primary_token) |>
-    httr2::req_url_query(!!!query_params)
 
-  if (stringr::str_length(res$url) > 4095) {
-    rlang::abort("Your request exceeds 4KB or 4096 characters, which is the upper limit of the Comtrade API.") # nolint
-  }
-
-  if (verbose) {
-    cli::cli_inform(c("i" = paste0("URL that will be queried: ", res$url)))
-  }
-
-  return(res)
+  return(req)
 }
