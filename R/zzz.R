ba076bf15262d2f37868cea628cc5b6fae1e884b
paul bochtler <65470117+datapumpernickel@users.noreply.github.com>
2024-05-23 13:33:04 +0200

    changed startup message to onattach and defaulted back to default rhub check

--- R/zzz.R
+++ R/zzz.R
@@ -12,6 +12,32 @@ assign(
   envir = ct_env
 )
 
+.onAttach <- function(libname, pkgname) {
+  ## throw a warning, if a cache in the non-compliant directory is detected
+  ## but only if there is actually any files in this directory
+  ## pointing to the migrating function
+  if ((
+    rappdirs::user_cache_dir('comtradr') !=
+    tools::R_user_dir('comtradr', which = 'cache')
+  ) &&
+  (length(list.files(rappdirs::user_cache_dir('comtradr'))) > 0 |
+   length(list.files(
+     rappdirs::user_cache_dir('comtradr_bulk')
+   )) > 0)) {
+    packageStartupMessage(cli::cli_bullets(
+      c(
+        "In the last version of comtradr the cache location has been changed, because it was not CRAN compliant. You can:", #nolint
+        '*' = 'Migrate the cache and clean old files with {.run ct_migrate_cache()}',
+        '*' = 'Ignore this warning, a new cache is created automatically.',
+        '*' = "Delete your old cache manually with:",
+        '*' = "{.run rappdirs::user_cache_dir('comtradr') |> list.files(full.names = T) |>   file.remove()}",  #nolint
+        ' ' = "and",
+        '*' = "{.run rappdirs::user_cache_dir('comtradr_bulk') |> list.files(full.names = T) |>   file.remove()}") #nolint
+    ))
+  }
+}
+
+
 
 .onLoad <- function(libname, pkgname) {
   max_size_env <- Sys.getenv("COMTRADR_CACHE_MAX_SIZE")
@@ -47,23 +73,7 @@ assign(
          value = cache,
          envir = rlang::ns_env("comtradr"))
 
-  ## throw a warning, if a cache in the non-compliant directory is detected
-  ## but only if there is actually any files in this directory
-  ## pointing to the migrating function
-  if(
-    (rappdirs::user_cache_dir('comtradr')!=
-     tools::R_user_dir('comtradr', which = 'cache')) &&
-    (length(list.files(rappdirs::user_cache_dir('comtradr')))>0|
-     length(list.files(rappdirs::user_cache_dir('comtradr_bulk')))>0)){
-    cli::cli_bullets(c("In the last version of comtradr the cache location has been changed, because it was not CRAN compliant. You can:", #nolint
-  '*' = 'Migrate the cache and clean old files with {.run ct_migrate_cache()}',
-  '*' = 'Ignore this warning, a new cache is created automatically.',
-  '*' = "Delete your old cache manually with:",
-  '*' = "{.run rappdirs::user_cache_dir('comtradr') |> list.files(full.names = T) |>   file.remove()}", #nolint
-  ' ' = "and",
-  '*' = "{.run rappdirs::user_cache_dir('comtradr_bulk') |> list.files(full.names = T) |>   file.remove()}") #nolint
-                     )
-  }
+
 
 }
 
