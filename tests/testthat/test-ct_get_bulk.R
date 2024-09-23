3867533cc6e049bcb1c027b9a2f85de3f5dffcd8
datapumpernickel <65470117+datapumpernickel@users.noreply.github.com>
2024-09-23 15:05:10 +0200

    refactor test code to be more parsimonious and print less verbose messages that might trip up the checking process on CRAN

--- tests/testthat/test-ct_get_bulk.R
+++ tests/testthat/test-ct_get_bulk.R
@@ -20,51 +20,60 @@ without_internet({
   test_that('URL query is correctly constructed with goods',
             {
               expect_GET(
-                comtradr::ct_get_bulk(reporter = 'ARG',
-                                      start_date = '1962',
-                                      end_date = "1962",
-                                      primary_token = 'test',
-                                      verbose = T)
-                ,'https://comtradeapi.un.org/bulk/v1/get/C/A/HS?reporterCode=32&period=1962') # nolint
+                comtradr::ct_get_bulk(
+                  reporter = 'ARG',
+                  start_date = '1962',
+                  end_date = "1962",
+                  primary_token = 'test'
+                ),
+                'https://comtradeapi.un.org/bulk/v1/get/C/A/HS?reporterCode=32&period=1962') # nolint
             })
 })
 
-httptest2::set_redactor(
-  function(x){
-    httptest2::gsub_response(x, "https\\://comtradeapi.un.org/bulk/v1/file/32/",
-                             "")
-    })
+httptest2::set_redactor(function(x) {
+  httptest2::gsub_response(x, "https\\://comtradeapi.un.org/bulk/v1/file/32/",
+                           "")
+})
 
 options(httptest2.verbose = TRUE)
 
+httptest2::with_mock_dir('../goods_bulk', {
+  test_that('returns a data.frame',
+            {
+              expect_s3_class(
+                comtradr::ct_get_bulk(
+                  reporter = 'ARG',
+                  start_date = '1962',
+                  end_date = "1962",
+                  primary_token = 'test',
+                  cache = FALSE
+                ),
+                'data.frame'
+              )
+            })
 
-httptest2::with_mock_dir('../goods_bulk',{
-    expect_s3_class(comtradr::ct_get_bulk(reporter = 'ARG',
-                                          start_date = '1962',
-                                          end_date = "1962",
-                                          primary_token = 'test',
-                                          cache = FALSE,
-                                          verbose = T), 'data.frame')
-})
+  test_that('returns correct verbose messages',
 
-httptest2::with_mock_dir("../goods_bulk",{
-    expect_message(comtradr::ct_get_bulk(reporter = 'ARG',
-                                         start_date = '1962',
-                                         end_date = "1962",
-                                         primary_token = 'test',
-                                         verbose = T),
-                   'Processing bulk file, this writes to your cache directory') # nolint
-    expect_message(comtradr::ct_get_bulk(reporter = 'ARG',
-                                         start_date = '1962',
-                                         end_date = "1962",
-                                         primary_token = 'test',
-                                         verbose = T),
-                   'Performing request, which can take a few seconds, depend') # nolint
+            {
+              captured_messages <- capture_messages({
+                comtradr::ct_get_bulk(
+                  reporter = 'ARG',
+                  start_date = '1962',
+                  end_date = "1962",
+                  primary_token = 'test',
+                  verbose = TRUE
+                )
+              })
 
-    expect_message(comtradr::ct_get_bulk(reporter = 'ARG',
-                                         start_date = '1962',
-                                         end_date = "1962",
-                                         primary_token = 'test',
-                                         verbose = T),
-                   'Will download files') # nolint
+              # Then you can use expect_match to verify the specific messages
+              expect_true(any(grepl(
+                "Processing bulk file", captured_messages
+              )))
+              expect_true(any(grepl(
+                "Performing request", captured_messages
+              )))
+              expect_true(any(grepl(
+                "Will download files", captured_messages
+              )))
+             })
 })
